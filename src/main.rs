b7f404dd24edb4cdbfa3465794c725995266e699
Jai <36728057+jaiherro@users.noreply.github.com>
2021-09-04 07:10:04 +1000

    init

---
+++ src/main.rs
@@ -0,0 +1,181 @@
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Client;
+use serde::Deserialize;
+use std::{
+    cmp::{self},
+    env,
+    path::Path,
+};
+use tokio::{
+    fs::{self, File},
+    io::AsyncWriteExt,
+};
+
+#[tokio::main]
+async fn main() {
+    // Get preferred version from arguments if it exists.
+    let args: Vec<String> = env::args().collect();
+
+    // Variables
+    let client = Client::new();
+    let version: String;
+
+    // Check for passed version to override automatic latest
+    if args.len() > 1 {
+        version = env::args().nth(1).unwrap();
+    } else {
+        version = get_version(&client).await.expect("Failed to get version!");
+    }
+
+    // Get latest build number for version
+    let build = get_build(&client, &version)
+        .await
+        .expect("Failed to get build!");
+
+    // Get file name for version and build number
+    let file = get_file(&client, &version, &build)
+        .await
+        .expect("Failed to get file!");
+
+    // Construct the URL
+    let url = format!(
+        "https://papermc.io/api/v2/projects/paper/versions/{}/builds/{}/downloads/{}",
+        version, build, file.0
+    );
+
+    // Check if a version_history.json exists, if so, read it and compare with latest version information
+    if Path::new("./version_history.json").exists() {
+        let version_history_contents = fs::read_to_string("./version_history.json").await.unwrap();
+        let version_history: VersionHistory =
+            serde_json::from_str(version_history_contents.as_str()).unwrap();
+        println!("{}", version_history.currentVersion);
+    }
+
+    download_file(&client, &url, &file.0).await.unwrap();
+}
+
+async fn get_version(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
+    client
+        .get("https://papermc.io/api/v2/projects/paper")
+        .send()
+        .await?
+        .json::<Version>()
+        .await?
+        .versions
+        .pop()
+        .ok_or_else(|| "needed at least one version".into())
+}
+
+async fn get_build(client: &Client, version: &String) -> Result<i64, Box<dyn std::error::Error>> {
+    client
+        .get(format!(
+            "https://papermc.io/api/v2/projects/paper/versions/{}",
+            version
+        ))
+        .send()
+        .await?
+        .json::<Build>()
+        .await?
+        .builds
+        .pop()
+        .ok_or_else(|| "needed at least one build".into())
+}
+
+async fn get_file(
+    client: &Client,
+    version: &String,
+    build: &i64,
+) -> Result<(String, String), Box<dyn std::error::Error>> {
+    let result = client
+        .get(format!(
+            "https://papermc.io/api/v2/projects/paper/versions/{}/builds/{}",
+            version, build
+        ))
+        .send()
+        .await?
+        .json::<Project>()
+        .await?;
+
+    Ok((
+        result.downloads.application.name,
+        result.downloads.application.sha256,
+    ))
+}
+
+async fn download_file(
+    client: &Client,
+    url: &String,
+    file_name: &String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Reqwest setup
+    let res = client
+        .get(url)
+        .send()
+        .await
+        .or(Err(format!("Failed to GET from '{}'", &url)))?;
+    let total_size = res
+        .content_length()
+        .ok_or(format!("Failed to get content length from '{}'", &url))?;
+
+    // Indicatif setup
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(&format!(
+        "Downloading {} from https://papermc.io/",
+        file_name
+    ));
+
+    // download chunks
+    let mut file = File::create("./paper.jar")
+        .await
+        .or(Err(format!("Failed to create file '{}'", file_name)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading {}", file_name)))?;
+        file.write(&chunk)
+            .await
+            .or(Err(format!("Error while writing to {}", file_name)))?;
+        let new = cmp::min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(&format!("Downloaded {}", file_name));
+    return Ok(());
+}
+
+#[derive(Deserialize)]
+struct Version {
+    versions: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct Build {
+    builds: Vec<i64>,
+}
+
+#[derive(Deserialize)]
+struct Project {
+    downloads: Downloads,
+}
+
+#[derive(Deserialize)]
+struct Downloads {
+    application: Application,
+}
+
+#[derive(Deserialize)]
+struct Application {
+    name: String,
+    sha256: String,
+}
+
+#[derive(Deserialize)]
+struct VersionHistory {
+    currentVersion: String,
+}
