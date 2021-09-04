66d4149fec34c05b50dac2ecf76af185975c8c21
Jai <36728057+jaiherro@users.noreply.github.com>
2021-09-05 00:56:10 +1000

    Added checksum

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,16 @@ use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
 use serde::Deserialize;
+use sha2::{Digest, Sha256};
 use std::{
     cmp::{self},
     env,
+    error::Error,
     path::Path,
 };
 use tokio::{
     fs::{self, File},
-    io::AsyncWriteExt,
+    io::{self, AsyncWriteExt},
 };
 
 #[tokio::main]
@@ -23,17 +25,21 @@ async fn main() {
 
     // Check for passed version to override automatic latest
     if args.len() > 1 {
+        println!("Argument passed, assuming version manually specified.");
         version = env::args().nth(1).unwrap();
     } else {
+        println!("Getting latest version.");
         version = get_version(&client).await.expect("Failed to get version!");
     }
 
     // Get latest build number for version
+    println!("Getting latest build number.");
     let build = get_build(&client, &version)
         .await
         .expect("Failed to get build!");
 
     // Get file name for version and build number
+    println!("Getting latest file name and SHA256 for verification.");
     let file = get_file(&client, &version, &build)
         .await
         .expect("Failed to get file!");
@@ -44,15 +50,27 @@ async fn main() {
         version, build, file.0
     );
 
+    // Construct the version comparison
+    let latest_version = format!("git-Paper-{} (MC: {})", build, version);
+    let version_history: VersionHistory;
+
     // Check if a version_history.json exists, if so, read it and compare with latest version information
     if Path::new("./version_history.json").exists() {
         let version_history_contents = fs::read_to_string("./version_history.json").await.unwrap();
-        let version_history: VersionHistory =
-            serde_json::from_str(version_history_contents.as_str()).unwrap();
-        println!("{}", version_history.currentVersion);
+        version_history = serde_json::from_str(version_history_contents.as_str()).unwrap();
+
+        if latest_version != version_history.currentVersion {
+            println!("Mismatch detected, updating...");
+            download_file(&client, &url, &file).await.unwrap();
+        } else {
+            println!("Server is already up-to-date.")
+        }
+    } else {
+        println!("Unable to verify server version, redownloading...");
+        download_file(&client, &url, &file).await.unwrap();
     }
 
-    download_file(&client, &url, &file.0).await.unwrap();
+    println!("All tasks completed, exiting.")
 }
 
 async fn get_version(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
@@ -106,7 +124,7 @@ async fn get_file(
 async fn download_file(
     client: &Client,
     url: &String,
-    file_name: &String,
+    file_information: &(String, String),
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Reqwest setup
     let res = client
@@ -125,27 +143,49 @@ async fn download_file(
         .progress_chars("#>-"));
     pb.set_message(&format!(
         "Downloading {} from https://papermc.io/",
-        file_name
+        file_information.0
     ));
 
     // download chunks
-    let mut file = File::create("./paper.jar")
-        .await
-        .or(Err(format!("Failed to create file '{}'", file_name)))?;
+    let mut file = File::create("./paper.jar").await.or(Err(format!(
+        "Failed to create file '{}'",
+        file_information.0
+    )))?;
     let mut downloaded: u64 = 0;
     let mut stream = res.bytes_stream();
 
     while let Some(item) = stream.next().await {
-        let chunk = item.or(Err(format!("Error while downloading {}", file_name)))?;
-        file.write(&chunk)
-            .await
-            .or(Err(format!("Error while writing to {}", file_name)))?;
+        let chunk = item.or(Err(format!(
+            "Error while downloading {}",
+            file_information.0
+        )))?;
+        file.write(&chunk).await.or(Err(format!(
+            "Error while writing to {}",
+            file_information.0
+        )))?;
         let new = cmp::min(downloaded + (chunk.len() as u64), total_size);
         downloaded = new;
         pb.set_position(new);
     }
 
-    pb.finish_with_message(&format!("Downloaded {}", file_name));
+    pb.finish_with_message(&format!("Downloaded {}", file_information.0));
+
+    // Verifying binary integrity
+    println!("Verifying binary integrity...");
+    let contents = fs::read("./paper.jar").await?;
+    let mut hasher = Sha256::new();
+    hasher.update(contents);
+    let hash_result = format!("{:X}", hasher.finalize()).to_lowercase();
+
+    if hash_result == file_information.1 {
+        println!("SHA256 checksums are identical, binary is perfect.")
+    } else {
+        return Err(
+            "SHA256 checksums do not match, deleting downloaded binary and invoking a panic."
+                .into(),
+        );
+    }
+
     return Ok(());
 }
 
