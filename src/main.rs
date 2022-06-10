27f0e097095f6d35df69fa6e89194d30aabb2c3a
Jai Herro <36728057+jaiherro@users.noreply.github.com>
2022-06-10 18:32:30 +1000

    Updated

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use tokio::{
     fs::{self, File},
     io::AsyncWriteExt,
 };
-use tracing::{info, subscriber, warn, Level};
+use tracing::{info, subscriber, warn, Level, error};
 use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
@@ -29,76 +29,95 @@ async fn main() {
 
     // Variables
     let client = Client::new();
-    let current_constructed_version: String;
-    let game_version: String;
-    let build_number: u16;
+    let version_history_current: String;
+    let wanted_version: String;
+    let wanted_build: u16;
 
-    // Check for passed version to override automatic latest
+    // Check for existing version
+    info!("Checking for current version information");
+    if Path::new("./version_history.json").exists() {
+        let version_history_contents = fs::read_to_string("./version_history.json").await.unwrap();
+        let version_history_json: VersionHistory =
+            serde_json::from_str(version_history_contents.as_str()).unwrap();
+        version_history_current = version_history_json.current_version;
+    } else {
+        version_history_current = "0.0.0".to_string();
+    }
+
+    // Check for passed version to override automatic latest, latest flag, or none of them
     if args.len() > 1 {
         match args[1].as_str() {
+            // Check for passed version
             "-v" | "--v" | "-version" | "--version" => {
-                info!("Specific version requested, skipping automatic latest");
-                game_version = args[2].clone();
+                info!("Version requested found: {}", &args[2]);
+                wanted_version = args[2].clone();
             }
+            // Check for latest flag
             "-l" | "--l" | "-latest" | "--latest" => {
                 info!("Absolute latest version requested, ignoring current version");
                 let mut all_game_versions: Vec<std::string::String> =
                     get_all_game_versions(&client)
                         .await
                         .expect("Failed to get versions");
-                game_version = latest_valid_version(&client, &mut all_game_versions)
+                wanted_version = latest_valid_version(&client, &mut all_game_versions)
                     .await
                     .expect("Couldn't get latest version");
             }
+            // If none of the above, panic
             _ => {
                 panic!("Unknown argument: {}", args[1]);
             }
         }
     } else {
-        if Path::new("./version_history.json").exists() {
-            let version_history_contents =
-                fs::read_to_string("./version_history.json").await.unwrap();
-            let version_history_json: VersionHistory =
-                serde_json::from_str(version_history_contents.as_str()).unwrap();
-            current_constructed_version = version_history_json.current_version;
-            game_version = current_constructed_version
-                .split("(MC: ")
-                .collect::<Vec<&str>>()[2]
-                .strip_suffix(")\"")
-                .expect("Failed to strip suffix")
-                .to_string();
-        } else {
-            info!("Unable to discern current server version");
+        // If version_history.json couldn't be found or read, use the latest version
+        if version_history_current == "0.0.0" {
+            info!("Current version not found, getting latest");
             let mut all_game_versions: Vec<std::string::String> = get_all_game_versions(&client)
                 .await
                 .expect("Failed to get versions");
-            game_version = latest_valid_version(&client, &mut all_game_versions)
+            wanted_version = latest_valid_version(&client, &mut all_game_versions)
                 .await
                 .expect("Couldn't get latest version");
+        // If version_history.json was found, extract the current version
+        } else {
+            info!("Current version found: {}", version_history_current);
+            wanted_version = version_history_current
+                .split("(MC: ")
+                .collect::<Vec<&str>>()[1]
+                .strip_suffix(")")
+                .expect("Failed to strip suffix")
+                .to_string();
         }
     }
 
-    build_number = get_build(&client, &game_version)
+    // Get the latest build number for a specific version
+    info!("Getting latest build for version: {}", wanted_version);
+    wanted_build = get_build(&client, &wanted_version)
         .await
         .expect("Failed to get build");
 
+    // Construct the version comparison string
+    let requested_constructed_version =
+        format!("git-Paper-{} (MC: {})", wanted_build, wanted_version);
+
     // Get file name for version and build number
-    info!("Getting latest file name and checksum");
-    let file = get_file(&client, &game_version, &build_number)
+    info!("Desired version is: {}", requested_constructed_version);
+    let file = get_file(&client, &wanted_version, &wanted_build)
         .await
         .expect("Failed to get file.");
 
     // Construct the URL
     let url = format!(
         "https://api.papermc.io/v2/projects/paper/versions/{}/builds/{}/downloads/{}",
-        game_version, build_number, file.0
+        wanted_version, wanted_build, file.0
     );
 
-    // Construct the version comparison string
-    let requested_constructed_version = format!("git-Paper-{} (MC: {})", build_number, game_version);
-
-    if current_constructed_version != requested_constructed_version {
-        info!("Updating server binary");
+    // Check if the version is already downloaded
+    if version_history_current != requested_constructed_version {
+        info!(
+            "Now downloading version {} build {}",
+            wanted_version, wanted_build
+        );
         download_file(&client, &url, &file).await.unwrap();
     } else {
         info!("Server is already up-to-date")
@@ -117,7 +136,10 @@ async fn latest_valid_version(
                 return Ok(version.clone().into());
             }
             Err(_) => {
-                warn!("No builds exist for {}, checking next latest", version);
+                warn!(
+                    "No builds exist for {}, checking next latest version",
+                    version
+                );
                 continue;
             }
         };
@@ -193,12 +215,8 @@ async fn download_file(
     // Indicatif setup
     let pb = ProgressBar::new(total_size);
     pb.set_style(ProgressStyle::default_bar()
-        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .template("{spinner:.white} [{wide_bar:.white}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .progress_chars("#>-"));
-    pb.set_message(format!(
-        "Downloading {} from https://api.papermc.io/",
-        file_information.0
-    ));
 
     // download chunks
     let mut file = File::create("./paper.jar").await.or(Err(format!(
@@ -211,39 +229,34 @@ async fn download_file(
     while let Some(item) = stream.next().await {
         let chunk = item.or(Err(format!(
             "Error while downloading {}",
-            file_information.0
+            &file_information.0
         )))?;
         file.write(&chunk).await.or(Err(format!(
             "Error while writing to {}",
-            file_information.0
+            &file_information.0
         )))?;
         let new = cmp::min(downloaded + (chunk.len() as u64), total_size);
         downloaded = new;
         pb.set_position(new);
     }
 
-    pb.finish_with_message(format!("Downloaded {}", file_information.0));
-
     // Verifying binary integrity
-    info!("Verifying hashes");
-    let contents = fs::read("./paper.jar").await?;
+    info!("Verifying file integrity");
+    let contents = fs::read("./paper.jar").await.expect("Failed to read downloaded file");
     let mut hasher = Sha256::new();
     hasher.update(contents);
     let hash_result = format!("{:X}", hasher.finalize());
-
-    if hash_result == file_information.1 {
-        info!("Hashes verified successfully, binary is authentic");
+    
+    if &hash_result == &file_information.1 {
+        info!("Hashes match, file is valid");
+        return Ok(());
     } else {
-        println!(
-            "The binary failed verification, assuming malicious and erasing to protect the host"
-        );
+        error!("Hashes do not match, downloaded binary may be corrupted, erasing file");
         fs::remove_file("./paper.jar")
             .await
-            .expect("Failed to remove file, this is very bad");
+            .expect("Failed to remove file");
         return Err("Binary failed hash verification".into());
     }
-
-    return Ok(());
 }
 
 #[derive(Deserialize)]
