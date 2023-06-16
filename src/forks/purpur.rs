c62a5732e9a885e3793e4ba1604d5116e10fc0cd
Jai <36728057+jaiherro@users.noreply.github.com>
2023-06-17 02:57:01 +1000

    more progress

---
+++ src/forks/purpur.rs
@@ -0,0 +1,91 @@
+use std::error::Error;
+
+use reqwest::Client;
+use serde::Deserialize;
+use tracing::info;
+
+use crate::VersionBuild;
+
+pub async fn get_latest_version_and_build(client: &Client) -> Result<VersionBuild, String> {
+    // Get the latest version and build information from the API
+    info!("Getting latest version and build information from API");
+    let latest_version_build: VersionBuild =
+        match client.get("https://api.pl3x.net/v2/purpur").send().await {
+            Ok(response) => match response.json::<VersionBuild>().await {
+                Ok(latest_version_build) => latest_version_build,
+                Err(_) => return Err("Failed to parse response".to_string()),
+            },
+            Err(_) => return Err("Failed to get response".to_string()),
+        };
+    info!("Latest version and build information found");
+
+    // Return the latest version and build as struct
+    Ok(latest_version_build)
+}
+
+pub async fn get_all_game_versions(client: &Client) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let version = client
+        .get("https://api.purpurmc.org/v2/purpur")
+        .send()
+        .await?
+        .json::<Purpur>()
+        .await?
+        .versions;
+
+    Ok(version)
+}
+
+pub async fn get_latest_build(
+    client: &Client,
+    version: &String,
+) -> Result<u16, Box<dyn std::error::Error>> {
+    let build = client
+        .get(format!("https://api.purpurmc.org/v2/purpur/{}", version).as_str())
+        .send()
+        .await?
+        .json::<Version>()
+        .await?
+        .builds
+        .all
+        .pop()
+        .unwrap();
+
+    Ok(build.parse::<u16>().unwrap())
+}
+
+pub async fn get_hash(
+    client: &Client,
+    version: &String,
+    build: &u16,
+) -> Result<String, Box<dyn Error>> {
+    let result = client
+        .get(format!("https://api.purpurmc.org/v2/purpur/{}/{}", version, build).as_str())
+        .send()
+        .await?
+        .json::<Build>()
+        .await?;
+
+    Ok(result.md5.to_uppercase())
+}
+
+// https://api.purpurmc.org/v2/purpur
+#[derive(Deserialize)]
+struct Purpur {
+    versions: Vec<String>,
+}
+
+// https://api.purpurmc.org/v2/purpur/{version}
+#[derive(Deserialize)]
+struct Version {
+    builds: Builds,
+}
+#[derive(Deserialize)]
+struct Builds {
+    all: Vec<String>,
+}
+
+// https://api.purpurmc.org/v2/purpur/{version}/{build} or https://api.purpurmc.org/v2/purpur/{version}/latest
+#[derive(Deserialize)]
+struct Build {
+    md5: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,26 @@ use clap::Parser;
 use reqwest::{Client, Response};
 use serde::Deserialize;
 use std::{
-    env,
     error::Error,
     fs::{self, File},
     io::Write,
-    ops::ControlFlow,
     path::Path,
-    time::Duration,
 };
 use tracing::{error, info, subscriber, warn, Level};
 use tracing_subscriber::FmtSubscriber;
 
+mod forks;
+
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
-    /// The game version to download (e.g. 1.20)
+    /// The game release to download (e.g. 1.20)
     #[arg(short, long, conflicts_with = "latest")]
-    version: String,
+    release: Option<String>,
 
-    /// Download the absolute latest version
-    #[arg(short, long, conflicts_with = "version")]
-    latest: bool,
+    /// Download the absolute latest release
+    #[arg(short, long, conflicts_with = "release")]
+    latest: Option<bool>,
 }
 
 #[tokio::main]
@@ -45,72 +44,87 @@ async fn main() {
     // Extract local version and build information from version_history.json
     let local_version_build: VersionBuild = match extract_local_version_and_build() {
         Ok(local_version_build) => local_version_build,
-        Err(error) => {
-            error!(
-                "Failed to extract local version and build information: {}",
-                error
-            );
-            panic!("Panicking due to failed extraction of local version and build information");
-        }
-    };
-
-    // Determine what version and build to download
-    let version_build: VersionBuild = if args.latest {
-        // Get the latest version and build information from the API
-        let latest_version_build: VersionBuild = match get_latest_version_and_build(&client).await {
-            Ok(latest_version_build) => latest_version_build,
-            Err(error) => {
-                error!(
-                    "Failed to get latest version and build information: {}",
-                    error
-                );
-                panic!(
-                    "Panicking due to failed extraction of latest version and build information"
-                );
-            }
-        };
-        // Check if the latest build is newer than the local build
-        if latest_version_build.version != local_version_build.version
-            && latest_version_build.build > local_version_build.build
-        {
-            info!("Latest version is newer than local version");
-            latest_version_build
-        } else {
-            info!("Latest version is not newer than local version");
-            local_version_build
-        }
-    } else {
-        // Check if the passed version is newer than the local version
-        if args.version > local_version_build.version {
-            info!("Passed version is newer than local version");
+        Err(_) => {
+            warn!("Failed to extract local version and build information");
             VersionBuild {
-                version: args.version,
+                version: "0.0".to_string(),
                 build: 0,
             }
-        } else {
-            info!("Passed version is not newer than local version");
-            local_version_build
         }
     };
 
-    // Check if the version and build are the same as the local version and build
-    if version_build.version == local_version_build.version
-        && version_build.build == local_version_build.build
-    {
-        info!("Version and build are the same as the local version and build");
-        info!("Skipping download");
-        return;
-    }
+    info!("Arg version: {:#?}", args.release);
+    info!("Arg latest: {:#?}", args.latest);
 
-    // Download the server.jar
-    info!("Downloading server.jar");
-    match download_server_jar(&client, version_build).await {
-        Ok(_) => info!("Downloaded server.jar"),
-        Err(error) => {
-            error!("Failed to download server.jar: {}", error);
-            panic!("Panicking due to failed download of server.jar");
-        }
-    };
+    // // Determine what version and build to download
+    // let version_build: VersionBuild = if args.latest {
+    //     // Get the latest version and build information from the API
+    //     let latest_version_build: VersionBuild = match forks::purpur::get_latest_version_and_build(&client).await {
+    //         Ok(latest_version_build) => latest_version_build,
+    //         Err(error) => {
+    //             error!(
+    //                 "Failed to get latest version and build information: {}",
+    //                 error
+    //             );
+    //             panic!(
+    //                 "Panicking due to failed extraction of latest version and build information"
+    //             );
+    //         }
+    //     };
+    //     // Check if the latest build is newer than the local build
+    //     if latest_version_build.version != local_version_build.version
+    //         && latest_version_build.build > local_version_build.build
+    //     {
+    //         info!("Latest version is newer than local version");
+    //         latest_version_build
+    //     } else {
+    //         info!("Latest version is not newer than local version");
+    //         local_version_build
+    //     }
+    // } else if args.version != "" {
+    //     // Check if the passed version is newer than the local version
+    //     if args.version > local_version_build.version {
+    //         info!("Passed version is newer than local version");
+    //         VersionBuild {
+    //             version: args.version,
+    //             build: 0,
+    //         }
+    //     } else {
+    //         info!("Passed version is not newer than local version");
+    //         local_version_build
+    //     }
+    // } else {
+    //     // Check if the passed version is newer than the local version
+    //     if args.version > local_version_build.version {
+    //         info!("Passed version is newer than local version");
+    //         VersionBuild {
+    //             version: args.version,
+    //             build: 0,
+    //         }
+    //     } else {
+    //         info!("Passed version is not newer than local version");
+    //         local_version_build
+    //     }
+    // };
+
+    // // Check if the version and build are the same as the local version and build
+    // if version_build.version == local_version_build.version
+    //     && version_build.build == local_version_build.build
+    // {
+    //     info!("Version and build are the same as the local version and build");
+    //     info!("Skipping download");
+    //     return;
+    // }
+
+    // // Download the server.jar
+    // info!("Downloading server.jar");
+    // match download_server_jar(&client, version_build).await {
+    //     Ok(_) => info!("Downloaded server.jar"),
+    //     Err(error) => {
+    //         error!("Failed to download server.jar: {}", error);
+    //         panic!("Panicking due to failed download of server.jar");
+    //     }
+    // };
 }
 
 fn extract_local_version_and_build() -> Result<VersionBuild, String> {
@@ -153,60 +167,17 @@ fn extract_local_version_and_build() -> Result<VersionBuild, String> {
     Ok(VersionBuild { version, build })
 }
 
-async fn get_latest_version_and_build(client: &Client) -> Result<VersionBuild, String> {
-    // Get the latest version and build information from the API
-    info!("Getting latest version and build information from API");
-    let latest_version_build: VersionBuild =
-        match client.get("https://api.pl3x.net/v2/purpur").send().await {
-            Ok(response) => match response.json::<VersionBuild>().await {
-                Ok(latest_version_build) => latest_version_build,
-                Err(_) => return Err("Failed to parse response".to_string()),
-            },
-            Err(_) => return Err("Failed to get response".to_string()),
-        };
-    info!("Latest version and build information found");
-
-    // Return the latest version and build as struct
-    Ok(latest_version_build)
-}
-
-async fn get_all_game_versions(client: &Client) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    let version = client
-        .get("https://api.purpurmc.org/v2/purpur")
-        .call()?
-        .into_json::<Purpur>()?
-        .versions;
-
-    Ok(version)
-}
-
-async fn get_latest_build(client: &Client, version: &String) -> Result<u16, Box<dyn std::error::Error>> {
-    let build = client
-        .get(format!("https://api.purpurmc.org/v2/purpur/{}", version).as_str())
-        .call()?
-        .into_json::<Version>()?
-        .builds
-        .all
-        .pop()
-        .unwrap();
-
-    Ok(build.parse::<u16>().unwrap())
-}
-
-async fn get_hash(client: &Client, version: &String, build: &u16) -> Result<String, Box<dyn Error>> {
-    let result = client
-        .get(format!("https://api.purpurmc.org/v2/purpur/{}/{}", version, build).as_str())
-        .call()?
-        .into_json::<Build>()?;
-
-    Ok(result.md5.to_uppercase())
-}
-
-fn download_file(client: &Client, url: &String, file_name: &str) -> Result<(), Box<dyn Error>> {
+async fn download_file(
+    client: &Client,
+    url: &String,
+    file_name: &str,
+) -> Result<(), Box<dyn Error>> {
     // Reqwest setup
-    let resp = client
+    let resp: Response = client
         .get(url)
-        .call()
+        .send()
+        .await?
+        .error_for_status()
         .or(Err(format!("Failed to GET from '{}'", &url)))?;
 
     // create file
@@ -214,9 +185,7 @@ fn download_file(client: &Client, url: &String, file_name: &str) -> Result<(), B
         .or(Err(format!("Failed to create file '{}'", file_name)))?;
 
     // write bytes to file
-    let mut reader = resp.into_reader();
-    let mut bytes = vec![];
-    reader.read_to_end(&mut bytes)?;
+    let bytes = resp.bytes().await?;
 
     file.write_all(&bytes)
         .or(Err(format!("Error while writing to {}", file_name)))?;
@@ -240,32 +209,11 @@ fn verify_binary(file_name: &str, hash: &String) -> Result<(), Box<dyn Error>> {
     }
 }
 
+// Standard version+build struct
 #[derive(Deserialize)]
 struct VersionBuild {
     version: String,
     build: u16,
-}
-
-// https://api.purpurmc.org/v2/purpur
-#[derive(Deserialize)]
-struct Purpur {
-    versions: Vec<String>,
-}
-
-// https://api.purpurmc.org/v2/purpur/{version}
-#[derive(Deserialize)]
-struct Version {
-    builds: Builds,
-}
-#[derive(Deserialize)]
-struct Builds {
-    all: Vec<String>,
-}
-
-// https://api.purpurmc.org/v2/purpur/{version}/{build} or https://api.purpurmc.org/v2/purpur/{version}/latest
-#[derive(Deserialize)]
-struct Build {
-    md5: String,
 }
 
 // version_history.json
